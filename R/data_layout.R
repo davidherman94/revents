5e75d4b27af7b3b198215d0589c8cca12f5fc2d3
David Herman <davidherman2704@gmail.com>
2024-07-10 13:42:50 +0200

    vignette

---
+++ R/data_layout.R
@@ -0,0 +1,23 @@
+#' Datos de ejemplo layout_1
+#'
+#' Una descripción de los datos de ejemplo layout_1.
+#'
+#' @format Un data frame con n filas y m columnas.
+#' @source La fuente de los datos
+"data_layout_1"
+
+#' Datos de ejemplo layout_2
+#'
+#' Una descripción de los datos de ejemplo layout_2.
+#'
+#' @format Un data frame con n filas y m columnas.
+#' @source La fuente de los datos
+"data_layout_2"
+
+#' Datos de ejemplo layout_3
+#'
+#' Una descripción de los datos de ejemplo layout_3.
+#'
+#' @format Un data frame con n filas y m columnas.
+#' @source La fuente de los datos
+"data_layout_3"
