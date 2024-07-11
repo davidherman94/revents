c862b7c412fe7aa3dbf992442aa19c0c05bb831a
David Herman <davidherman2704@gmail.com>
2024-07-11 09:42:42 +0200

    vignette update)

--- R/data_layout.R
+++ R/data_layout.R
@@ -1,6 +1,17 @@
 #' Datos layout_1 for conditional and marginal rates models
 #'
-#'
+#'   \item{ID}{participant ID}
+#'   \item{PARAMCD}{parameter}
+#'   \item{TSTART}{time study start}
+#'   \item{TSTOP}{time stop observation}
+#'   \item{TGAP}{time gap between events}
+#'   \item{STATUS}{status of the event}
+#'   \item{SEVENT}{sequence number of event per id}
+#'   \item{AGE}{age of the participant}
+#'   \item{SEX}{Sex of the participant}
+#'   \item{DISEASE_COURSE}{type of disease course}
+#'   \item{RACE}{race of the participant}
+#'   \item{TIME_SINCE_DIAGNOSIS}{time since diagnosis}#'
 #'
 #' @format Un data frame con 6347 rows y 13 columns.
 #' @source data simulated from MSOAC parameters
@@ -8,7 +19,18 @@
 
 #' Data layout_2 for marginal hazard models
 #'
-#'
+#'   \item{ID}{participant ID}
+#'   \item{PARAMCD}{parameter}
+#'   \item{TSTART}{time study start}
+#'   \item{TSTOP}{time stop observation}
+#'   \item{TGAP}{time gap between events}
+#'   \item{STATUS}{status of the event}
+#'   \item{SEVENT}{sequence number of event per id}
+#'   \item{AGE}{age of the participant}
+#'   \item{SEX}{Sex of the participant}
+#'   \item{DISEASE_COURSE}{type of disease course}
+#'   \item{RACE}{race of the participant}
+#'   \item{TIME_SINCE_DIAGNOSIS}{time since diagnosis}
 #'
 #' @format a data frame with con 15765 rows y 12 columns.
 #' @source data simulated from MSOAC parameters
@@ -16,7 +38,15 @@
 
 #' Data layout_3 for count-based models
 #'
-#'
+#'   \item{ID}{participant ID}
+#'   \item{PARAMCD}{parameter}
+#'   \item{COUNT}{number of confirmed events}
+#'   \item{SEVENT}{sequence number of event per id}
+#'   \item{AGE}{age of the participant}
+#'   \item{SEX}{Sex of the participant}
+#'   \item{DISEASE_COURSE}{type of disease course}
+#'   \item{RACE}{race of the participant}
+#'   \item{TIME_SINCE_DIAGNOSIS}{time since diagnosis}
 #'
 #' @format Un data frame con 1313 rows y 9 columns.
 #' @source data simulated from MSOAC parameters
