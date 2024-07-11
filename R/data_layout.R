a4213c7819618d6154e38dff07ec717365600138
David Herman <davidherman2704@gmail.com>
2024-07-11 16:48:06 +0200

    vignette update

--- R/data_layout.R
+++ R/data_layout.R
@@ -1,5 +1,9 @@
-#' Datos layout_1 for conditional and marginal rates models
+#' Data layout_1
 #'
+#' This dataset contains information about participants, including demographic details and event-related data.
+#'
+#' @format A data frame with 6347 rows and 13 columns:
+#' \describe{
 #'   \item{ID}{participant ID}
 #'   \item{PARAMCD}{parameter}
 #'   \item{TSTART}{time study start}
@@ -8,46 +12,51 @@
 #'   \item{STATUS}{status of the event}
 #'   \item{SEVENT}{sequence number of event per id}
 #'   \item{AGE}{age of the participant}
-#'   \item{SEX}{Sex of the participant}
-#'   \item{DISEASE_COURSE}{type of disease course}
-#'   \item{RACE}{race of the participant}
-#'   \item{TIME_SINCE_DIAGNOSIS}{time since diagnosis}#'
-#'
-#' @format Un data frame con 6347 rows y 13 columns.
-#' @source data simulated
-"data_layout_1"
-
-#' Data layout_2 for marginal hazard models
-#'
-#'   \item{ID}{participant ID}
-#'   \item{PARAMCD}{parameter}
-#'   \item{TSTART}{time study start}
-#'   \item{TSTOP}{time stop observation}
-#'   \item{TGAP}{time gap between events}
-#'   \item{STATUS}{status of the event}
-#'   \item{SEVENT}{sequence number of event per id}
-#'   \item{AGE}{age of the participant}
-#'   \item{SEX}{Sex of the participant}
+#'   \item{SEX}{sex of the participant}
 #'   \item{DISEASE_COURSE}{type of disease course}
 #'   \item{RACE}{race of the participant}
 #'   \item{TIME_SINCE_DIAGNOSIS}{time since diagnosis}
+#' }
+#' @source Simulated data for conditional and marginal rates models
+"data_layout_1"
+
+#' Data layout_2
 #'
-#' @format a data frame with con 15765 rows y 12 columns.
-#' @source data simulated
+#' This dataset contains information about participants, including demographic details and event-related data.
+#'
+#' @format A data frame with 15765 rows and 12 columns:
+#' \describe{
+#'   \item{ID}{participant ID}
+#'   \item{PARAMCD}{parameter}
+#'   \item{TSTART}{time study start}
+#'   \item{TSTOP}{time stop observation}
+#'   \item{TGAP}{time gap between events}
+#'   \item{STATUS}{status of the event}
+#'   \item{SEVENT}{sequence number of event per id}
+#'   \item{AGE}{age of the participant}
+#'   \item{SEX}{sex of the participant}
+#'   \item{DISEASE_COURSE}{type of disease course}
+#'   \item{RACE}{race of the participant}
+#'   \item{TIME_SINCE_DIAGNOSIS}{time since diagnosis}
+#' }
+#' @source Simulated data for marginal hazard models
 "data_layout_2"
 
-#' Data layout_3 for count-based models
+#' Data layout_3
 #'
+#' This dataset contains information about participants, including demographic details and event-related data.
+#'
+#' @format A data frame with 1313 rows and 9 columns:
+#' \describe{
 #'   \item{ID}{participant ID}
 #'   \item{PARAMCD}{parameter}
 #'   \item{COUNT}{number of confirmed events}
 #'   \item{SEVENT}{sequence number of event per id}
 #'   \item{AGE}{age of the participant}
-#'   \item{SEX}{Sex of the participant}
+#'   \item{SEX}{sex of the participant}
 #'   \item{DISEASE_COURSE}{type of disease course}
 #'   \item{RACE}{race of the participant}
 #'   \item{TIME_SINCE_DIAGNOSIS}{time since diagnosis}
-#'
-#' @format Un data frame con 1313 rows y 9 columns.
-#' @source data simulated
+#' }
+#' @source Simulated data for count-based models
 "data_layout_3"
